942941bd72b10b9941e013683aac32f2eadb2e25
soyeonkim0404 <58216604+soyeonkim0404@users.noreply.github.com>
2020-09-14 13:40:55 +0900

    2020-09-14
    update

---
+++ project2020/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <myHeader />
+    <myContent msg="Welcome to Your Vue.js App"/>
+    <myFooter />
+  </div>
+</template>
+
+<script>
+import myHeader from './components/myHeader.vue'
+import myContent from './components/myContent.vue'
+import myFooter from './components/myFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    myHeader,
+    myContent,
+    myFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
